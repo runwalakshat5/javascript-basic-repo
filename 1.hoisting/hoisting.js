697a919e9579325ca8d9744b90bf9b4189083c96
runwalakshat5 <runwalakshat5@gmail.com>
2022-02-23 15:23:51 +0530

    added theory file

---
+++ 1.hoisting/hoisting.js
@@ -0,0 +1,27 @@
+console.log(a)
+
+var a =10;
+
+
+b()
+
+function b(){
+    console.log('Hello b')
+}
+
+var x =10;
+var x;
+console.log(x)
+
+// capture varibale keep in memeber assign a  default value called undefined
+// a=undefined
+
+// function b(){
+
+// }
+
+// x = undefined
+// x = undefined 
+
+// x=10
+// console.log(10)
